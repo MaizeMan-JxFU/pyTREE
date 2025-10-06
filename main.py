05faf14869cb19d4e1d72fc37bd4ffe1b013688a
Jingxian <827888724@qq.com>
2025-10-06 18:17:07 +0800

    Fixed some bugs

--- main.py
+++ main.py
@@ -8,18 +8,19 @@ class TREE:
         self._getchild(Treedic)
         plt.savefig('test.png')
         pass
-    def _getchild(self,dic:dict,loc:tuple=(-1,0)):
+    def _getchild(self,dic:dict,loc:tuple=(-1,0),layer:int=1):
         x,y = loc
         for ind,root in enumerate(dic.keys()):
-            print(root)
+            print(root,layer)
             child = dic[root]
-            x = loc[0]+(-1)**(ind)
+            x = loc[0]+(-1)**(ind)/layer
             y = loc[1]+child[0]
-            print(x,y)
+            print([loc[0],x],[loc[1],y])
             # self.ax.text(*loc,root,ha='center')
             self.ax.text(x,y,root,ha='center',va='center')
             self.ax.scatter(x,y,color='red')
-            self._getchild(child[1],(x,y))
+            self.ax.plot([loc[0],x],[loc[1],y],color='black')
+            self._getchild(child[1],(x,y),layer+1)
 
 # nwk file (A:0.1,B:0.2,(C:0.3, D:0.4)E:0.5)F to dictionary
 tree_dict = {
