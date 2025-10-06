c1e7f09f0d3af058932e558507b790b2f34c91ee
Jingxian <827888724@qq.com>
2025-10-06 18:05:48 +0800

    Creat package

---
+++ main.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class TREE:
+    def __init__(self,Treedic: dict):
+        print(Treedic)
+        self.ax = plt.gca()
+        self._getchild(Treedic)
+        plt.savefig('test.png')
+        pass
+    def _getchild(self,dic:dict,loc:tuple=(-1,0)):
+        x,y = loc
+        for ind,root in enumerate(dic.keys()):
+            print(root)
+            child = dic[root]
+            x = loc[0]+(-1)**(ind)
+            y = loc[1]+child[0]
+            print(x,y)
+            # self.ax.text(*loc,root,ha='center')
+            self.ax.text(x,y,root,ha='center',va='center')
+            self.ax.scatter(x,y,color='red')
+            self._getchild(child[1],(x,y))
+
+# nwk file (A:0.1,B:0.2,(C:0.3, D:0.4)E:0.5)F to dictionary
+tree_dict = {
+    'S':[0,
+        {'A':[0.1,
+              {'E':[0.2,{}],
+               'F':[0.5,{}]}],
+         'B':[0.5,
+              {'C':[0.3,{}],
+               'D':[0.4,{}]}]}]
+    }
+TREE(tree_dict)
