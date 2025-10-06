99a23f21b8b74c8cf1628c51717475958bc8f28e
Jingxian <827888724@qq.com>
2025-10-06 18:46:17 +0800

    Fixed some bugs

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import pandas as pd
+from sklearn import tree
 class TREE:
     def __init__(self,Treedic: dict):
-        print(Treedic)
-        self.ax = plt.gca()
+        self._ = []
         self._getchild(Treedic)
-        plt.savefig('test.png')
         pass
     def _getchild(self,dic:dict,loc:tuple=(0,0),layer:int=1):
         x,y = loc
         for ind,root in enumerate(dic.keys()):
-            print(root,layer)
             child = dic[root]
             x,y = (loc[0]+(-1)**(ind)/layer,loc[1]+child[0]) if layer > 1 else (x,y)
-            print([loc[0],x],[loc[1],y])
-            self.ax.text(x,y,root,ha='center',va='center')
-            self.ax.scatter(x,y,color='red')
-            self.ax.plot([loc[0],x],[loc[1],y],color='black')
+            self._.append([root,loc[0],x,loc[1],y])
             self._getchild(child[1],(x,y),layer+1)
 
 # nwk file (A:0.1,B:0.2,(C:0.3, D:0.4)E:0.5)F to dictionary
 tree_dict = {
     'S':[0,
         {'A':[0.1,
-              {'E':[0.2,{}],
+              {'E':[0.2,{'Test':[0.1,{}]}],
                'F':[0.5,{}]}],
          'B':[0.5,
               {'C':[0.3,{}],
                'D':[0.4,{}]}]}]
     }
-TREE(tree_dict)
+model = TREE(tree_dict)
+tree_df = pd.DataFrame(model._)
+# x_loc = sorted(set(tree_df[1].unique().tolist()+tree_df[2].unique().tolist()))
+# x_loc_trans_dict = dict(zip(x_loc,range(len(x_loc))))
+# tree_df[1] = tree_df[1].map(x_loc_trans_dict)
+# tree_df[2] = tree_df[2].map(x_loc_trans_dict)
+for i in tree_df.index:
+    plt.plot(tree_df.loc[i][[1,2]],tree_df.loc[i][[3,4]],color='black')
+    pass
+plt.savefig('test.png')
