0ec305e63864c89d74c57d14a1644616c211ec9c
Jingxian <827888724@qq.com>
2025-10-09 11:11:58 +0800

    Everything will be ok

--- nwk2dict.py
+++ nwk2dict.py
@@ -22,14 +22,68 @@ class NWKKIT:
 class NWK:
     def __init__(self,nwk:str):
         nwk = nwk.replace(' ','').replace(';','').strip()
-        self.pattern = r'\([^()]*\)' # 匹配最内侧子树
-        nwk = self._get_innerNode(nwk)
+        nwk = self._recode(nwk)
         print(nwk)
+        self.pattern = r'\([^()]*\)' # 匹配最内侧子树
+        self.tree = {}
+        while '(' in nwk:
+            nwk = self._get_innerNode(nwk)
+        print(len(self.tree))
+        print(self.tree)
         pass
-    def _get_innerNode(self,nwk):
-        re.search(self.pattern,nwk).group()
+    def _get_innerNode(self,nwk:str):
+        innerNode = re.search(self.pattern,nwk).group()
+        nwk_after = nwk[nwk.index(innerNode):]
+        Node1 = re.search(rf'\)(.*?)\)',nwk_after)
+        Node2 = re.search(rf'\)(.*?)\,',nwk_after)
+        Node = None
+        parentNode = None
+        if Node1 != None:
+            Node1 = Node1.group()[1:-1]
+            if Node1.count(':')==1:
+                Node = Node1 
+        if Node2 != None:
+            Node2 = Node2.group()[1:-1]
+            if Node2.count(':')==1:
+                Node = Node2
+        if Node != None: # 存在节点才有父节点
+            nwk_after_after = nwk_after[nwk_after.index(Node):]
+            parentNode1 = re.search(rf'\)(.*?)\)',nwk_after_after)
+            parentNode2 = re.search(rf'\)(.*?)\,',nwk_after_after)
+            if parentNode1 != None:
+                parentNode1 = parentNode1.group()[1:-1]
+                if parentNode1.count(':')==1:
+                    parentNode = parentNode1 
+            if parentNode2 != None:
+                parentNode2 = parentNode2.group()[1:-1]
+                if parentNode2.count(':')==1:
+                    parentNode = parentNode2
+            self.tree[Node.split(':')[0]] = [Node.split(':')[1],innerNode[1:-1].split(','),parentNode]
+        else:
+            parentNode = None
+            self.tree['root'] = [None,innerNode[1:-1].split(','),None]
+        nwk = nwk.replace(innerNode,'')
         return nwk
-    
+    def _recode(self, nwk):
+        pattern = r'([(,)])([^(]*?):'
+        counter = 1
+        mapping = {}
+
+        def replacement(match):
+            nonlocal counter
+            prefix = match.group(1)
+            content = match.group(2)
+            # 为当前匹配分配索引
+            index = counter
+            counter += 1
+            # 记录映射
+            mapping[index] = content
+            return f"{prefix}{index}:"
+        new_nwk = re.sub(pattern, replacement, nwk)
+        self.mapping = mapping
+        return new_nwk
+
+
 with open('tree.test.nwk') as f:
     nwk = f.readline().strip()
 t = time.time()
